20107a6a7ebdc15e322aac039002a3d77b0d2650
george uy <georgeduy29@gmail.com>
2022-01-28 15:27:49 +0800

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => ['required'],
+            'password' => ['required']
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'username' => ['The Provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken($request->header('user_agent'))->plainTextToken;
+
+        $response = [
+            'token' => $token,
+            'user' => $user,
+        ];
+
+        return response()->json($response);
+    }
+
+    public function register(Request $request)
+    {   
+        $request->validate([
+            'fullname' => ['required'],
+            'username' => ['required'],
+            'password' => ['required']
+        ]);
+
+        $user = User::create($request->all());
+
+        $response = [
+            'message' => 'Successfully Registered',
+            'user' => $user,
+        ];
+
+        return response()->json($response);
+    }
+
+    public function logout(Request $request)
+    {
+        auth('sanctum')->user()->tokens()->delete();
+        
+        return response()->json('logout', 201);
+    }
+
+}
